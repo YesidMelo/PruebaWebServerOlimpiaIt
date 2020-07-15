01a110327100703b6cd5fd84f29891d239dcdadb
Yesid <trigoperfec3@hotmail.com>
2020-07-15 16:47:41 -0500

    Adicion de validaciones de usuario enviado al servidor

--- app.js
+++ app.js
@@ -1,11 +1,117 @@
-var express = require('express');
+const express = require('express');
+const bodyParser = require('body-parser');
+
 var app = express();
 
-var puerto = 3000;
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
 
+var puerto = 3000;
+const formatoRespuesta = 'json';
 app.post('/',function(solicitud, respuesta){
-    respuesta.send("Hola mundo");
+    respuesta.type(formatoRespuesta)
+    if(!esValidoElUsuario(solicitud,respuesta)){ return; }
+    guardarEnDBUsuario(solicitud,respuesta);
 });
+
+function esValidoElUsuario(solicitud,respuesta){
+    
+    if(solicitud.body.nombre == null || solicitud.body.nombre ==="" ){ 
+        var jsonRespuesta = {
+            codigo : 1,
+            mensaje : "No ha ingresado Un nombre"
+        }
+        respuesta.send(jsonRespuesta);
+        return false; 
+    }
+
+    if(solicitud.body.cedula == null || solicitud.body.cedula ==="" ){ 
+        var jsonRespuesta = {
+            codigo : 1,
+            mensaje : "No ha ingresado una cedula"
+        }
+        respuesta.send(jsonRespuesta);
+        return false; 
+    }
+
+    if(solicitud.body.direccion == null || solicitud.body.direccion ==="" ){ 
+        var jsonRespuesta = {
+            codigo : 1,
+            mensaje : "No ha ingresado una direccion"
+        }
+        respuesta.send(jsonRespuesta);
+        return false; 
+    }
+
+    if(solicitud.body.ciudad == null || solicitud.body.ciudad ==="" ){ 
+        var jsonRespuesta = {
+            codigo : 1,
+            mensaje : "No ha ingresado una ciudad"
+        }
+        respuesta.send(jsonRespuesta);
+        return false; 
+    }
+
+    if(solicitud.body.pais == null || solicitud.body.pais ==="" ){ 
+        var jsonRespuesta = {
+            codigo : 1,
+            mensaje : "No ha ingresado un pais"
+        }
+        respuesta.send(jsonRespuesta);
+        return false; 
+    }
+
+    if(solicitud.body.celular == null || solicitud.body.celular ==="" ){ 
+        var jsonRespuesta = {
+            codigo : 1,
+            mensaje : "No ha ingresado un celular"
+        }
+        respuesta.send(jsonRespuesta);
+        return false; 
+    }
+
+    if(solicitud.body.foto == null || solicitud.body.foto ==="" ){ 
+        var jsonRespuesta = {
+            codigo : 1,
+            mensaje : "No ha ingresado un foto"
+        }
+        respuesta.send(jsonRespuesta);
+        return false; 
+    }
+
+    if(solicitud.body.GPS == null ){ 
+        var jsonRespuesta = {
+            codigo : 1,
+            mensaje : "No ha ingresado un posicion geografica"
+        }
+        respuesta.send(jsonRespuesta);
+        return false; 
+    }
+
+    if(solicitud.body.Bluetooth == null || solicitud.body.Bluetooth ===  ""){ 
+        var jsonRespuesta = {
+            codigo : 1,
+            mensaje : "No ha ingresado un estado de bluetooth"
+        }
+        respuesta.send(jsonRespuesta);
+        return false; 
+    }
+    
+    if(solicitud.body.Wifi == null || solicitud.body.Wifi ===  ""){ 
+        var jsonRespuesta = {
+            codigo : 1,
+            mensaje : "No ha ingresado un estado de Wifi"
+        }
+        respuesta.send(jsonRespuesta);
+        return false; 
+    }
+
+    return true;
+}
+
+function guardarEnDBUsuario(solicitud,respuesta){
+    
+}
 
 app.get('/',function(solicitud, respuesta){
     respuesta.send("Hola mundo");
